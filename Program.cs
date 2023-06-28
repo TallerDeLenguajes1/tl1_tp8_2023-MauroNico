059b06bfb6ed1dc54c8b2f86b18573f81c914a5a
MauroNico <mauronico432@gmail.com>
2023-06-28 06:26:49 -0300

    segundo commit

--- Program.cs
+++ Program.cs
@@ -2,76 +2,104 @@
 using System.Collections.Generic;
 using System.IO;
 
+class Tarea
+{
+    public int TareaID { get; set; }
+    public string Descripcion { get; set; }
+    public int Duracion { get; set; }
 
-List<Tarea> pendientes = new List<Tarea>();
-List<Tarea> realizadas = new List<Tarea>();
-Random rnd = new Random();
-int N;
-Console.WriteLine("Ingrese el numero de tareas a realizar\n");
-N = Convert.ToInt32(Console.ReadLine());
-string penal;
-for(int i = 0;i<N;i++){
-    Tarea tarea = new Tarea();
-    tarea.TareaID = i+1;
-    Console.WriteLine($"Ingrese la descripcion de la tarea {tarea.TareaID}");
-    penal = Console.ReadLine();
-    tarea.Descripcion = penal;
-    Console.WriteLine($"Ingrese la duracion de la tarea {tarea.TareaID}");
-    tarea.duracion = Convert.ToInt32(Console.ReadLine());
-    pendientes.Add(tarea);
-}
-Console.WriteLine("PENDIENTES");
-foreach(Tarea tarea in pendientes)
-{
-    Console.WriteLine($"TareaID= {tarea.TareaID}");
-    Console.WriteLine($"Descripcion: {tarea.Descripcion}");
-    Console.WriteLine($"Duracion: {tarea.duracion}\n");
-}
-Console.WriteLine("Que item desea mover?");
-N = Convert.ToInt32(Console.ReadLine());
-Tarea item = pendientes[(N-1)];
-realizadas.Add(item);
-pendientes.Remove(item);
-
-Console.WriteLine("\n\nPENDIENTES");
-foreach(Tarea tarea in pendientes)
-{
-    Console.WriteLine($"TareaID= {tarea.TareaID}");
-    Console.WriteLine($"Descripcion: {tarea.Descripcion}");
-    Console.WriteLine($"Duracion: {tarea.duracion}\n");
-}
-Console.WriteLine("\n\nREALIZADAS");
-foreach(Tarea tarea in realizadas)
-{
-    Console.WriteLine($"TareaID= {tarea.TareaID}");
-    Console.WriteLine($"Descripcion: {tarea.Descripcion}");
-    Console.WriteLine($"Duracion: {tarea.duracion}\n");
-}
-string busca;
-Console.WriteLine("\n\nIngrese la descripcion de la tarea a buscar");
-busca = Console.ReadLine();
-foreach(Tarea tarea in pendientes)
-{
-    if(tarea.Descripcion == busca)
+    public Tarea(int tareaID, string descripcion, int duracion)
     {
-        Console.WriteLine($"TareaID= {tarea.TareaID}");
-        Console.WriteLine($"Descripcion: {tarea.Descripcion}");
-        Console.WriteLine($"Duracion: {tarea.duracion}\n");
+        TareaID = tareaID;
+        Descripcion = descripcion;
+        Duracion = duracion;
     }
 }
-int horasTrabajadas = 0;;
-foreach(Tarea tarea in pendientes){
-    horasTrabajadas += tarea.duracion;
-}
-string nombreArchivo = "horas_trabajadas.txt";
-StreamWriter archivo = new StreamWriter(nombreArchivo);
 
-archivo.WriteLine("Sumario de horas trabajadas:");
-archivo.WriteLine(horasTrabajadas);
-
-public class Tarea
+class Empleado
 {
-    public int TareaID;
-    public string Descripcion;
-    public int duracion;
+    public List<Tarea> TareasPendientes { get; set; }
+    public List<Tarea> TareasRealizadas { get; set; }
+
+    public Empleado()
+    {
+        TareasPendientes = new List<Tarea>();
+        TareasRealizadas = new List<Tarea>();
+    }
+
+    public void AgregarTareaPendiente(Tarea tarea)
+    {
+        TareasPendientes.Add(tarea);
+    }
+
+    public void MoverTareaPendienteARealizada(int tareaID)
+    {
+        Tarea tarea = TareasPendientes.Find(t => t.TareaID == tareaID);
+        if (tarea != null)
+        {
+            TareasPendientes.Remove(tarea);
+            TareasRealizadas.Add(tarea);
+            Console.WriteLine("La tarea se ha movido de pendiente a realizada.");
+        }
+        else
+        {
+            Console.WriteLine("No se encontró una tarea con el ID especificado en las tareas pendientes.");
+        }
+    }
+
+    public List<Tarea> BuscarTareasPendientesPorDescripcion(string descripcion)
+    {
+        return TareasPendientes.FindAll(t => t.Descripcion.Contains(descripcion));
+    }
+
+    public void GuardarSumarioHorasTrabajadas(string nombreArchivo)
+    {
+        int horasTrabajadas = 0;
+        foreach (Tarea tarea in TareasRealizadas)
+        {
+            horasTrabajadas += tarea.Duracion;
+        }
+
+        using (StreamWriter writer = new StreamWriter(nombreArchivo, true))
+        {
+            writer.WriteLine($"Horas trabajadas: {horasTrabajadas}");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Empleado empleado = new Empleado();
+
+        
+        Tarea tarea1 = new Tarea(1, "Completar informe", 60);
+        Tarea tarea2 = new Tarea(2, "Enviar correo electrónico", 15);
+        Tarea tarea3 = new Tarea(3, "Preparar presentación", 45);
+        Tarea tarea4 = new Tarea(4, "Realizar llamada telefónica", 10);
+        Tarea tarea5 = new Tarea(5, "Actualizar base de datos", 30);
+
+        
+        empleado.AgregarTareaPendiente(tarea1);
+        empleado.AgregarTareaPendiente(tarea2);
+        empleado.AgregarTareaPendiente(tarea3);
+        empleado.AgregarTareaPendiente(tarea4);
+        empleado.AgregarTareaPendiente(tarea5);
+
+        
+        int tareaIDMovida = 3; // ID de la tarea a mover
+        empleado.MoverTareaPendienteARealizada(tareaIDMovida);
+
+        
+        string descripcionBusqueda = "correo";
+        List<Tarea> tareasEncontradas = empleado.BuscarTareasPendientesPorDescripcion(descripcionBusqueda);
+        Console.WriteLine($"Se encontraron {tareasEncontradas.Count} tareas pendientes que contienen '{descripcionBusqueda}' en su descripción.");
+
+        
+        string nombreArchivo = "sumario_horas_trabajadas.txt";
+        empleado.GuardarSumarioHorasTrabajadas(nombreArchivo);
+
+        Console.WriteLine("Se ha guardado el sumario de horas trabajadas en el archivo.");
+    }
 }
